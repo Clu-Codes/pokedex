ab123cd47ac8a20daf5a61c6092fa238628e1667
Clu-Codes <retsulcconnor@gmail.com>
2024-06-30 08:42:20 -0400

    refactored main.go code into separate components; adder helper functions for commands and sanitizing text

---
+++ repl.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	reader := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		reader.Scan()
+
+		input := cleanText(reader.Text())
+		if len(input) == 0 {
+			continue
+		}
+
+		cmd := input[0]
+
+		cmdName, exists := getCommands()[cmd]
+		if exists {
+			err := cmdName.callback()
+			if err != nil { // redundant given schema for cliCommand struct, but keeping just in case.
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
+	}
+}
+
+func cleanText(text string) []string {
+	output := strings.ToLower(text)
+	words := strings.Fields(output)
+	return words
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exits the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
